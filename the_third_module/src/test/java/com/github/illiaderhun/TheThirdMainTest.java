9abaed64c129a6087ff497a70f9bb497043c3d7f
Illia Derhun <illya.dergum@gmail.com>
2018-11-24 19:20:21 +0200

    All tests are green

--- the_third_module/src/test/java/com/github/illiaderhun/TheThirdMainTest.java
+++ the_third_module/src/test/java/com/github/illiaderhun/TheThirdMainTest.java
@@ -9,7 +9,7 @@ public class TheThirdMainTest {
     @Test
     public void sub() {
         TheThirdMain theThirdMain = new TheThirdMain();
-        int expected = 5;
+        int expected = 6;
         int actual = theThirdMain.sub(2, 3);
         assertEquals(expected, actual);
     }
